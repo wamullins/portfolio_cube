9bd5efe07443385463bbf051e7bb6de3fba9ea04
Andrew <mullinsa428@gmail.com>
2023-07-24 00:07:28 -0400

    small resume change

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -124,7 +124,7 @@ export const Resume = () => {
             textColor: "black"
         },
         {
-            text: " Undergraduate Researcher, VUMC Department of Neurology | Nashville, TN",
+            text: "Undergraduate Researcher, VUMC Department of Neurology | Nashville, TN",
             boxFormat: {
                 height:0.1,
                 marginLeft:-2
